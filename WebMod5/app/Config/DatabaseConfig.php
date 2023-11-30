7ac956c34bf60e8f68183cd5796d127c2067544c
inastsabitaah <127664527+inastsabitaah@users.noreply.github.com>
2023-11-30 10:04:39 +0700

    Add files via upload

---
+++ WebMod5/app/Config/DatabaseConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\Config;
+
+class DatabaseConfig
+{
+    // setting database
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "latprakwebmod5";
+    public $port = 3306;
+}
